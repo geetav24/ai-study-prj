792bebcbee84fbc448c03a2a3769ff2d92600b8f
Geeta Vatkar <geetavatkar@Geetas-MacBook-Pro.local>
2025-12-30 22:54:32 -0500

    hello-world ollama

--- langchain-course/main.py
+++ langchain-course/main.py
@@ -1,5 +1,9 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
 def main():
-    print("Hello from langchain-course!")
+    print("Hello from ai-study-prj!")
+    print(os.environ.get("OPENAI_API_KEY"))
 
 
 if __name__ == "__main__":
